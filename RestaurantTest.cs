6da4b680af74e03faac7d820f6c4ebf05ba43018
Tot0sh <monnier.ru@gmail.com>
2021-12-24 11:54:34 +0100

    Add files via upload

---
+++ RestaurantTest.cs
@@ -0,0 +1,47 @@
+﻿using LeGrandRestaurant.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeGrandRestaurant
+{
+    class RestaurantTest
+    {
+        private static Serveur[] serveurs = new ServeurGenerator().Generate(3).ToArray();
+        private static Restaurant restaurant = new Restaurant(serveurs);
+
+        public void command()
+        {
+            decimal prixCommande = 34.6M;
+
+            foreach(Serveur serveur in serveurs)
+                serveur.PrendreCommande(prixCommande);
+
+            restaurant.ChiffreAffaires.Equals(prixCommande * serveurs.Length);
+        }
+
+        public void commandPrixZero()
+        {
+            decimal prixCommande = 0;
+
+            foreach (Serveur serveur in serveurs)
+                serveur.PrendreCommande(prixCommande);
+
+            restaurant.ChiffreAffaires.Equals(prixCommande * serveurs.Length);
+        }
+
+        public void commandPrixNegatif()
+        {
+            decimal prixCommande = -10.7M;
+
+            foreach (Serveur serveur in serveurs)
+                serveur.PrendreCommande(prixCommande);
+
+            restaurant.ChiffreAffaires.Equals(prixCommande * serveurs.Length);
+        }
+
+    }
+
+}

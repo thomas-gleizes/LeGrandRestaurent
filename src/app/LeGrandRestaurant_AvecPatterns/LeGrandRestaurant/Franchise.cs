96721fe19ce8278ad4a0737cefff0f86a6f4ffaa
Tot0sh <monnier.ru@gmail.com>
2022-02-07 20:15:32 +0100

    Add files via upload

---
+++ src/app/LeGrandRestaurant_AvecPatterns/LeGrandRestaurant/Franchise.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace LeGrandRestaurant
+{
+    public class Franchise
+    {
+        private readonly Restaurant[] _restaurants;
+
+        public Franchise(params Restaurant[] restaurants)
+        {
+            _restaurants = restaurants;
+        }
+
+        public decimal ChiffreAffaires
+            => _restaurants.Sum(restaurant => restaurant.ChiffreAffaires);
+    }
+}
